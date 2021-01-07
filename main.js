4b7a26ba114b143fff84d157d5f65584882de029
etanfoo <etanfoo@gmail.com>
2021-01-07 17:17:06 +1100

    added button functionality

---
+++ main.js
@@ -0,0 +1,21 @@
+// var num_one = document.getElementsByTagName("input")[0];
+// var num_two = document.getElementsByTagName("input")[1];
+
+
+// num_one.addEventListener("input", add);
+// num_two.addEventListener("input", add);
+
+// function add() {
+//     var one = parseFloat(num_one.value) || 0;
+//     var two = parseFloat(num_two.value) || 0;
+
+//     // alert("your sum is:" + (one + two));
+// }
+
+
+var button = document.getElementById("calculate");
+button.onclick = function() {
+
+    alert("test");
+
+};
